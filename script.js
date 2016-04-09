3cc166ea1a3b8b85d24811b268be548668bfe194
avoytovich <levanwork@gmail.com>
2016-04-09 10:02:00 +0000

    Initial commit

--- script.js
+++ script.js
@@ -2,10 +2,31 @@
 
 document.getElementById("decor").addEventListener("click", myFunction);
 
-
 function myFunction() {
-    var enterprise = prompt('Enter name of your enterprise:');
-    document.getElementById("demo").innerHTML = "Production of " + enterprise 
-    document.getElementById("demo").style.background="#ffffff"
+    var name = prompt('Please, enter your name');
+    document.getElementById("demo").innerHTML = "...Thank's, " + name + ".<br> I am glad to see you...,if you wanna listen to <br> music, click on button in left-top corner like this <img src='images/android-icon-36x36.png'>..."
+    document.getElementById("demo1").style.height="350px"
+    document.getElementById("demo").style.padding="7%"
+    document.getElementById("demo").style.fontSize="61px"
+    document.getElementById("demo").style.color="#4CAF50"
     document.getElementById("decor").removeEventListener("click", myFunction);
+}
+
+document.getElementById("decor1").addEventListener("click", myAudio);
+
+function myAudio() {
+    var audio = document.getElementById("demo2");
+    audio.play();
+}
+
+document.getElementById("decor2").addEventListener("click", myAnimation);
+
+function myAnimation() {
+    document.getElementById("html").style.animation = "mymoveh 5s 1"
+    document.getElementById("css").style.animation = "mymovec 5s 1"
+    document.getElementById("javascript").style.animation = "mymovej 5s 1"
+    document.getElementById("demo").innerHTML = "It is time for search information <br>in the navigation. I hope, you hire me :)"
+    document.getElementById("html").innerHTML = "HTML: a good"
+    document.getElementById("css").innerHTML = "CSS: not bad"
+    document.getElementById("javascript").innerHTML = "JavaScript:"
 }
